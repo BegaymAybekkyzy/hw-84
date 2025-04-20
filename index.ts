e8a94854e74eed32e3a38d62195c8fd789170dab
Aybek kyzy Begaym <ajbekkyzybegajym1@gmail.com>
2025-04-20 21:52:51 +0600

    Инициализация проекта

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+const run = async () => {
+    app.listen(port, () => {
+        console.log(`Server started on http://localhost:${port}`);
+    });
+};
+
+run().catch((console.error));
