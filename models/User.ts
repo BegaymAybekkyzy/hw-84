6646d826e7c93fe899f29cdf0704b962b595ce53
Aybek kyzy Begaym <ajbekkyzybegajym1@gmail.com>
2025-04-21 20:59:52 +0600

    Реализована логика аутентификации и авторизации пользователя

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,29 @@
 import * as mongoose from "mongoose";
+import * as argon2 from "argon2";
+import {randomUUID} from "node:crypto";
+import {HydratedDocument, Model} from "mongoose";
+import {IUser} from "../types";
 
-const UserSchema = new mongoose.Schema({
+interface UserMethods {
+    checkPassword: (password: string) => Promise<boolean>;
+    generateToken: () => void;
+}
+
+const ARGON2_OPTIONS = {
+    type: argon2.argon2id,
+    memoryCost: 2 ** 16,
+    timeCost: 5,
+    parallelism: 1
+}
+
+type UserModel = Model<IUser, {}, UserMethods>;
+
+const UserSchema = new mongoose.Schema<
+    HydratedDocument<IUser>,
+    UserModel,
+    UserMethods,
+    {}
+>({
     username: {
         type: String,
         required: true
@@ -14,6 +37,26 @@ const UserSchema = new mongoose.Schema({
         required: true,
     }
 });
+
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await argon2.hash(this.password, ARGON2_OPTIONS);
+});
+
+UserSchema.methods.checkPassword = async function (password: string) {
+    return await argon2.verify(this.password, password);
+};
+
+UserSchema.methods.generateToken = async function () {
+    this.token = randomUUID();
+};
+
+UserSchema.set("toJSON", {
+    transform: (_doc, ret) => {
+        delete ret.password;
+        return ret;
+    }
+})
 
 const User = mongoose.model("User", UserSchema);
 export default User;
--- routers/users.ts
+++ routers/users.ts
@@ -1,5 +1,54 @@
 import express from "express";
+import User from "../models/User";
+import {Error} from "mongoose";
 
 const userRouter = express.Router();
+
+userRouter.post("/", async (req, res, next) => {
+    try {
+        const newUser = new User({
+            username: req.body.username,
+            password: req.body.password,
+        });
+        newUser.generateToken();
+        await newUser.save();
+        res.send(newUser);
+    } catch (error) {
+        if (error instanceof Error.ValidationError) {
+            res.status(400).send(error);
+            return;
+        }
+        next(error);
+    }
+});
+
+userRouter.post("/sessions", async (req, res, next) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+
+        if (!user) {
+            res.status(400).send({message: "User not found"});
+            return;
+        }
+
+        if(!req.body.password){
+            res.status(400).send({error: "Enter your password"});
+            return;
+        }
+
+        const isMatch = await user.checkPassword(req.body.password);
+
+        if (!isMatch) {
+            res.status(400).send({error: "Invalid password"});
+            return;
+        }
+
+        user.generateToken();
+        await user.save();
+        res.send({message: "Username and password correct", user});
+    } catch (error) {
+        next(error);
+    }
+});
 
 export default userRouter;
